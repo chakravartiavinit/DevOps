7aa91a1e22236b73672771a054767c80ea2eb9c7
Chakravarti Avinit <chakravartiavinit@Chakravartis-MacBook-Air.local>
2022-12-30 00:48:33 +0530

    Added Git and Github File

---
+++ Git.cpp
@@ -0,0 +1,12 @@
+/**
+ * Git and Github
+ * 
+ * 
+ * 
+ * 
+ * git stash -To send your current files to stash
+ * git stash pop -To get back the files at stash
+ * git stash clear-To clean all the files currently in the stash 
+ * 
+ * 
+*/
