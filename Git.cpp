36a9880704799e1c2f4a0cb57a60927621f76070
Chakravarti Avinit <chakravartiavinit@Chakravartis-MacBook-Air.local>
2022-12-30 00:54:23 +0530

    Done Changes

--- Git.cpp
+++ Git.cpp
@@ -8,5 +8,11 @@
  * git stash pop -To get back the files at stash
  * git stash clear-To clean all the files currently in the stash 
  * 
+ * To attach remote repository with the Github repo
+ * git remote add origin [URL]
+ * 
+ * 
+ * 
+ * 
  * 
 */
